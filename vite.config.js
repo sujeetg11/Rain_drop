d01dcb20f628a841182c875b7e0273e5a45b9db0
sujeetg11 <sujeetgupta_21411@aitpune.edu.in>
2025-06-02 01:34:29 +0530

    deploy issues

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
+// base: Command === 'build' ? '/Rain_drop/' : '/',
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
+  base: '/Rain_drop/',
   server: {
     port: 3000,
     open: true
